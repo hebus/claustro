ceaef135744f82dd676d41f608f3d5adf43f5fe7
hebus <olivier@Mac-mini-de-Olivier.local>
2012-06-16 11:26:40 +0200

    Initial Commit

---
+++ claustro/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  claustro
+//
+//  Created by Christina on 16/06/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ claustro/MasterViewController.h
@@ -0,0 +1,13 @@
+//
+//  MasterViewController.h
+//  claustro
+//
+//  Created by Christina on 16/06/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MasterViewController : UITableViewController
+
+@end
